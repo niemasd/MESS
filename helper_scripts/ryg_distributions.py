ddff8161b255b1b731383e69c394cbd75043f25f
Niema Moshiri <niemamoshiri@gmail.com>
2023-06-01 18:54:58 -0700

    Added smooth KDE before histogram

--- helper_scripts/ryg_distributions.py
+++ helper_scripts/ryg_distributions.py
@@ -9,8 +9,8 @@ Plot the distribution of red, yellow, and green counts across all pairs of stude
 
 # imports
 from os.path import isfile
-from pandas import DataFrame
-from seaborn import histplot, move_legend
+from matplotlib.backends.backend_pdf import PdfPages
+from seaborn import kdeplot, histplot
 from sys import argv, stderr
 import matplotlib.pyplot as plt
 
@@ -29,7 +29,6 @@ if __name__ == "__main__":
         error("Output file exists: %s" % argv[2])
 
     # load responses
-    #data = list(); hue = list()
     red, yellow, green = list(), list(), list()
     with open(argv[1]) as tsv:
         for row_num, l in enumerate(tsv):
@@ -37,18 +36,32 @@ if __name__ == "__main__":
                 continue
             r, y, g = [int(v.strip()) for v in l.split('\t')[6:9]]
             red.append(r); yellow.append(y); green.append(g)
-            #data += [int(v.strip()) for v in l.split('\t')[6:9]]
-            #hue += ["Same Wrong Answer", "Different Wrong Answers", "Only 1 Missed"]
+    data = [
+        (red, 'red', "Same Wrong Answer"),
+        (yellow, 'yellow', "Different Wrong Answers"),
+        (green, 'green', "Only 1 Missed"),
+    ]
+    max_val = max(max(red),max(yellow),max(green))
+    bins = list(range(max_val+1))
 
     # plot distributions
-    fig, ax = plt.subplots(figsize=(10,5))
-    max_val = max(max(red),max(yellow),max(green)); bins = list(range(max_val+1))
-    histplot(data=red, bins=bins, color='red', label="Same Wrong Answer")
-    histplot(data=yellow, bins=bins, color='yellow', label="Different Wrong Answers")
-    histplot(data=green, bins=bins, color='green', label="Only 1 Missed")
-    plt.xlim(xmin=0, xmax=max_val)
-    plt.title("Red, Yellow, and Green Distributions")
-    plt.xlabel("Number of Questions")
-    plt.ylabel("Number of Pairs of Students")
-    plt.legend(bbox_to_anchor=(0.995, 0.995), loc=1, borderaxespad=0., ncol=1)
-    plt.savefig(argv[2], format='pdf', bbox_inches='tight')
+    with PdfPages(argv[2]) as pdf:
+        for plot_type in [kdeplot, histplot]:
+            fig, ax = plt.subplots(figsize=(10,5))
+            for vals, color, label in data:
+                if plot_type is histplot:
+                    ylabel = "Number of Pairs"
+                    plot_type(data=vals, color=color, label=label, bins=bins)
+                else:
+                    if color == 'yellow':
+                        color = 'goldenrod'
+                    ylabel = "Proportion of Pairs"
+                    plot_type(data=vals, color=color, label=label, bw_adjust=5)
+            plt.xlim(xmin=0, xmax=max_val)
+            plt.title("Red, Yellow, and Green Distributions")
+            plt.xlabel("Number of Questions")
+            plt.ylabel(ylabel)
+            plt.legend(bbox_to_anchor=(0.995, 0.995), loc=1, borderaxespad=0., ncol=1)
+            plt.tight_layout()
+            pdf.savefig(plt.gcf())
+            plt.cla(); plt.clf(); plt.close('all')
