f50f7795ec1509559ab2a5bdbe03823e6509a4e8
Niema Moshiri <niemamoshiri@gmail.com>
2023-06-07 10:08:02 -0700

    Added RYG plot

--- helper_scripts/ryg_distributions.py
+++ helper_scripts/ryg_distributions.py
@@ -10,7 +10,7 @@ Plot the distribution of red, yellow, and green counts across all pairs of stude
 # imports
 from os.path import isfile
 from matplotlib.backends.backend_pdf import PdfPages
-from seaborn import kdeplot, histplot
+from seaborn import histplot, kdeplot, regplot
 from sys import argv, stderr
 import matplotlib.pyplot as plt
 
@@ -29,23 +29,25 @@ if __name__ == "__main__":
         error("Output file exists: %s" % argv[2])
 
     # load responses
-    red, yellow, green = list(), list(), list()
+    red, yellow, green, total_wrong = list(), list(), list(), list()
     with open(argv[1]) as tsv:
         for row_num, l in enumerate(tsv):
             if row_num == 0:
                 continue
             r, y, g = [int(v.strip()) for v in l.split('\t')[6:9]]
-            red.append(r); yellow.append(y); green.append(g)
+            red.append(r); yellow.append(y); green.append(g); total_wrong.append(r+y+g)
     data = [
         (red, 'red', "Same Wrong Answer"),
         (yellow, 'yellow', "Different Wrong Answers"),
         (green, 'green', "Only 1 Missed"),
     ]
     max_val = max(max(red),max(yellow),max(green))
+    max_tot = max(total_wrong)
     bins = list(range(max_val+1))
 
     # plot distributions
     with PdfPages(argv[2]) as pdf:
+        # plot KDE and histogram
         for plot_type in [kdeplot, histplot]:
             fig, ax = plt.subplots(figsize=(10,5))
             for vals, color, label in data:
@@ -61,7 +63,19 @@ if __name__ == "__main__":
             plt.title("Red, Yellow, and Green Distributions")
             plt.xlabel("Number of Questions")
             plt.ylabel(ylabel)
-            plt.legend(bbox_to_anchor=(0.995, 0.995), loc=1, borderaxespad=0., ncol=1)
+            plt.legend(bbox_to_anchor=(0.995, 0.995), loc='upper right', borderaxespad=0., ncol=1)
             plt.tight_layout()
-            pdf.savefig(plt.gcf())
-            plt.cla(); plt.clf(); plt.close('all')
+            pdf.savefig(plt.gcf()); plt.cla(); plt.clf(); plt.close('all')
+
+        # plot RYG vs. total wrong
+        fig, ax = plt.subplots(figsize=(10,5))
+        for vals, color, label in data:
+            regplot(x=total_wrong, y=vals, color=color, label=label, ci=None, scatter_kws={'alpha':0.1})
+        plt.xlim(xmin=0, xmax=max_tot)
+        plt.ylim(ymin=0, ymax=max_val)
+        plt.title("Red, Yellow, and Green vs. Total Wrong")
+        plt.xlabel("Total Number of Questions Either Student Got Wrong (Red+Yellow+Green)")
+        plt.ylabel("Number of Questions Red, Yellow, or Green")
+        plt.legend(bbox_to_anchor=(0.005, 0.995), loc='upper left', borderaxespad=0., ncol=1)
+        plt.tight_layout()
+        pdf.savefig(plt.gcf()); plt.cla(); plt.clf(); plt.close('all')
